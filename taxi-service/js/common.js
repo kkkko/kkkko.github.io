371f8503c837118e73f9809f58a447392bf2c392
kkkko <mr.feshov@mail.ru>
2017-11-01 20:33:19 +0300

    new project

---
+++ taxi-service/js/common.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+	$(".header__logo").animated("fadeInDown", "fadeOutDown");
+	$(".header__nav-bar-menu").animated("fadeInDown", "fadeOutDown");
+
+	$(".top__title").animated("fadeIn", "fadeOutDown");
+	$(".top__number").animated("fadeIn", "fadeOutDown");
+	$(".top__url").animated("fadeIn", "fadeOutDown");
+	$(".car-big").animated("fadeInLeft", "fadeOut");
+
+	$(".about__title_top").animated("fadeInDown", "fadeOutDown");
+	$(".about__title_bottom").animated("fadeInDown", "fadeOutDown");
+	$(".about__text").animated("fadeInDown", "fadeOutDown");
+	$(".about__btn").animated("fadeInDown", "fadeOut");
+
+	$(".booking").animated("fadeInUp", "fadeOut");
+
+	$(".container-name").animated("fadeInDown", "fadeOut");
+
+	$(".economy__class").animated("fadeInLeft", "fadeOut");
+	$(".standard__class").animated("fadeInUp", "fadeOut");
+	$(".business__class").animated("fadeInRight", "fadeOut");
+
+	$(".advantages-content-left").animated("fadeInLeft", "fadeOut");
+	$(".advantages-content-right").animated("fadeInRight", "fadeOut");
+
+	$(".advantages__btn").animated("fadeInUp", "fadeOut");
+
+	$(".apps_left h2").animated("fadeInDown", "fadeOut");
+	$(".apps_left p").animated("fadeInDown", "fadeOut");
+	$(".google__btn img").animated("fadeInLeft", "fadeOut");
+	$(".ios__btn img").animated("fadeInLeft", "fadeOut");
+
+	$(".apps_right").animated("fadeInRight", "fadeOut");
+
+	$(".clients-left").animated("fadeInLeft", "fadeOut");
+	$(".clients-right").animated("fadeInRight", "fadeOut");
+
+	$(".call-wrapper h3").animated("fadeIn", "fadeOut");
+	$(".call-wrapper h2").animated("Pulse", "fadeOut");
+
+	$(".bottom-col-left").animated("fadeInLeft", "fadeOut");
+	$(".bottom-col-center").animated("fadeIn", "fadeOut");
+	$(".bottom-col-right").animated("fadeInRight", "fadeOut");
+
+});
